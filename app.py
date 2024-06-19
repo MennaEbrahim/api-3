30e5f8bee209c671964b16fa5ff319a8faf87e09
MennaEbrahim <90441040+MennaEbrahim@users.noreply.github.com>
2024-06-19 20:56:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ async def predict_next_week_consumption(request: ConsumptionRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=10000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
